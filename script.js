45b98d56f88a804f5bb9a8a21d92a1da4a540c02
kamackay <kamackay@ncsu.edu>
2016-06-30 20:24:08 -0400

    splash page isn't totally awful

--- script.js
+++ script.js
@@ -17,5 +17,68 @@ $(document).ready(function () {
                 break;
         }
     }); /**/
-    $('#contents').append('<img class="full full-stretch" src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/material_dark.jpg" />')
+    $('#contents').append('<img id="page-back" class="full" style="z-index: 100;opacity:0" src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/material_dark.jpg" />');
+    back = $('#page-back');
+    back.load(function () {
+        back.animate({
+            opacity: 100
+        }, {
+            duration: 2000,
+            queue: false,
+            complete: dropIn
+        });
+    });
+    var r = function () {
+        var w = back.width(),
+            w2 = $(window).width();
+        if (w < w2) back.css('width', w2);
+    };
+    $(window).resize(r);
+    r();
 });
+var back;
+
+function dropIn() {
+    var els = $('.dropIn');
+    $.each(els, function (n, o) {
+        var t = $(this);
+        t.animate({
+            top: t.attr('drop-top')
+        }, {
+            duration: t.attr('drop-time') || 2000,
+            queue: false,
+            easing: 'easeOutBounce'
+        });
+    });
+    setTimeout(dropUp, 2000);
+}
+
+function dropUp() {
+    var els = $('.dropUp');
+    $.each(els, function (n, o) {
+        var t = $(this);
+        t.animate({
+            top: t.attr('drop-top') || 0
+        }, {
+            duration: t.attr('drop-time') || 2000,
+            queue: false,
+            easing: 'easeOutBounce',
+            complete: bounceUp
+        });
+    });
+}
+
+function bounceUp() {
+    var els = $('.bounceUp');
+    $.each(els, function (n, o) {
+        var t = $(this);
+        t.animate({
+            top: t.attr('drop-top') || 0
+        }, {
+            duration: t.attr('drop-time') || 2000,
+            queue: false,
+            easing: 'easeOutBounce',
+            complete: bounceUp
+        });
+    });
+}
