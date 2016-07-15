b7b64df5eaff21a7a530df7f5123d771b203a8ac
Keith <kamackay@ncsu.edu>
2016-07-15 16:26:40 -0400

    nothin major

--- game/script.js
+++ game/script.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
     $.each($('body').find('*'), function (n, o) {
         $(this).addClass('unselectable');
     });
+    var begin = function () {
+        toast('This page is not ready yet, please feel free to return to http://keithmackay.com');
+    }
+    $(document).on('mousedown', begin);
+    $(document).on('keydown', begin);
 });
