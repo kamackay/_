d03062aaf57e14345c834eacd1e0a6fbed7565f6
Keith <kamackay@ncsu.edu>
2016-07-18 10:09:24 -0400

    update

--- game/script.js
+++ game/script.js
@@ -1,12 +1,22 @@
+var started = false;
+var curs;
 $(document).ready(function () {
     collapseSettings('fast');
     removeContextMenu();
+    curs = $('#cursorImg')
     $.each($('body').find('*'), function (n, o) {
         $(this).addClass('unselectable');
     });
     var begin = function () {
-        toast('This page is not ready yet, please feel free to return to http://keithmackay.com');
+        if (!started) {
+            toast('This page is not ready yet, please feel free to return to http://keithmackay.com');
+            started = true;
+            $('#pressToBegin').fadeOut();
+        }
     }
     $(document).on('mousedown', begin);
     $(document).on('keydown', begin);
+    $('*').mousemove(function (e) {
+        curs.css('top', e.pageY).css('left', e.pageX);
+    });
 });
