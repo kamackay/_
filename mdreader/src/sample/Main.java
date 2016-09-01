6f8a9f242cd513ba1cb53a5c185a63492db45d7f
Keith MacKay <kamackay@ncsu.edu>
2016-09-01 13:28:00 -0400

    Start of Markdown conversion

--- mdreader/src/sample/Main.java
+++ mdreader/src/sample/Main.java
@@ -15,13 +15,25 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main extends Application {
     private final WebView browser = new WebView();
 
+    /*
+     * TODO:
+         * Tables
+         * "-------" => Line
+         * Line Underlined with "=====" => <h1>
+         * Line Underlined with "-----" => <h2>
+         * "*" => strong
+         * "**" or "__" => much stronger
+         * "_" => italics
+         * "~" => strike through
+         * Lists, numbered and un-numbered
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         GridPane grid = new GridPane();
@@ -54,10 +66,15 @@ public class Main extends Application {
                 (int) (screenBounds.getHeight() * .75), screenBounds.getWidth() * .4));
         primaryStage.show();
         try {
-            List<String> args = getParameters().getRaw();
+            ArrayList<String> args = new ArrayList<>();
             args.add("C:\\Documents\\Google Drive\\School\\Computer Graphics\\notes\\ray-casting-notes.md");
-            args.stream().filter(arg -> arg.toLowerCase().endsWith(".md") && new File(arg).exists()).forEach(this::loadFile);
-        } catch (Exception e){
+            args.addAll(getParameters().getRaw());
+            for (String arg : args)
+                if (arg.toLowerCase().endsWith(".md") && new File(arg).exists()) {
+                    loadFile(arg);
+                    break;
+                }
+        } catch (Exception e) {
             updateHtml(e.getMessage());
         }
     }
@@ -101,27 +118,75 @@ public class Main extends Application {
         protected String call() throws Exception {
             StringBuilder sb = new StringBuilder("<div id='mainDiv' style='color:white;height:100vh;width:100vw;'>");
             String[] lines = markdownString.split("[\\r\\n]+");
-            for (String l : lines) {
-                String fLine = l;
-                if (fLine.matches("^#+\\s.*")) {
-                    if (fLine.startsWith("# "))
-                        fLine = "<h1>" + fLine.replaceAll("^#+\\s", "") + "</h1>";
-                    else if (fLine.startsWith("## "))
-                        fLine = "<h2>" + fLine.replaceAll("^#+\\s", "") + "</h2>";
-                    else if (fLine.startsWith("### "))
-                        fLine = "<h3>" + fLine.replaceAll("^#+\\s", "") + "</h3>";
-                    else if (fLine.startsWith("#### "))
-                        fLine = "<h4>" + fLine.replaceAll("^#+\\s", "") + "</h4>";
-                    else if (fLine.startsWith("##### "))
-                        fLine = "<h5>" + fLine.replaceAll("^#+\\s", "") + "</h5>";
-                    else if (fLine.startsWith("###### "))
-                        fLine = "<h6>" + fLine.replaceAll("^#+\\s", "") + "</h6>";
+            FormatState state = new FormatState();
+            for (int i = 0; i < lines.length; i++) {
+                String fLine = lines[i];
+                formatting:
+                {
+                    if (fLine.matches("\\s*")){
+                        break formatting;
+                    }
+                    if (fLine.matches("^#+\\s.*")) {
+                        if (fLine.startsWith("# "))
+                            fLine = "<h1>" + fLine.replaceAll("^#+\\s", "") + "</h1>";
+                        else if (fLine.startsWith("## "))
+                            fLine = "<h2>" + fLine.replaceAll("^#+\\s", "") + "</h2>";
+                        else if (fLine.startsWith("### "))
+                            fLine = "<h3>" + fLine.replaceAll("^#+\\s", "") + "</h3>";
+                        else if (fLine.startsWith("#### "))
+                            fLine = "<h4>" + fLine.replaceAll("^#+\\s", "") + "</h4>";
+                        else if (fLine.startsWith("##### "))
+                            fLine = "<h5>" + fLine.replaceAll("^#+\\s", "") + "</h5>";
+                        else if (fLine.startsWith("###### "))
+                            fLine = "<h6>" + fLine.replaceAll("^#+\\s", "") + "</h6>";
+                    }
+                    if (fLine.matches("^\\d+\\.\\s.*")) {
+                        //Numbered list, like 1.
+                        String[] sStr = fLine.split("\\.\\s", 2);
+                        if (sStr.length == 2) {
+                            if (state.ol) {
+                                fLine = String.format(Locale.getDefault(), "<li>%s</li>", sStr[1]);
+                            } else {
+                                state.ol = true;
+                                fLine = String.format(Locale.getDefault(),
+                                        "<ol start='%s'><li>%s", sStr[0], sStr[1]);
+                            }
+                        }
+                    } else if (state.ol){
+                        state.ol = false;
+                        fLine = "</ol>" + fLine;
+                    }
+                    if (fLine.matches("^\\s*[-⋅*+]\\s.*")){
+                        //Unordered List
+                        String lineText = fLine.replaceAll("^\\s*[-⋅*+]\\s", "");
+                        if (state.ul){
+                            fLine = String.format(Locale.getDefault(),
+                                    "<li>%s</li>", lineText);
+                        } else {
+                            state.ol = true;
+                            fLine = String.format(Locale.getDefault(),
+                                    "<ul><li>%s</li>", lineText);
+                        }
+                    } else if (state.ul){
+                        state.ul = false;
+                        fLine = "</ul>" + fLine;
+                    }
                 }
-                sb.append(fLine);
-                sb.append("<br>");
+                lines[i] = fLine;
             }
+            for (String t : lines) sb.append(t);
             sb.append("</div>");
             return sb.toString();
+        }
+    }
+
+    private class FormatState {
+        public boolean ol, ul, code, table;
+
+        public FormatState() {
+            ol = false;
+            ul = false;
+            code = false;table = false;
         }
     }
 
