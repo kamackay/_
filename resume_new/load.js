9ddc00236a175248dd02859f3470c2d6198daa30
kamackay <kamackay@ncsu.edu>
2016-10-03 23:43:11 -0400

    change

--- resume_new/load.js
+++ resume_new/load.js
@@ -1,26 +1,28 @@
 (function () {
-    var loadJs = function (js, index) {
-        index = index || 0;
-        _get({
-            url: js[index],
-            done: function (a) {
-                var s = document.createElement("script");
-                s.type = "text/javascript";
-                s.innerHTML = a;
-                document.head.appendElement(s);
-                loadJs(js, index++);
-            }
-        });
-    };
+  var loadJs = function (js, index) {
+    index = index || 0
     _get({
-        url: './load.json',
-        done: function (data) {
-            var b = '';
-            const head = document.getElementById('head');
-            data.css.forEach(function (a) {
-                head.innerHTML += '<link rel="stylesheet" href="' + a + '">';
-            });
-            loadJs(data.js, 0);
-        }
-    });
+      url: js[index],
+      done: function (a) {
+        var names = js[index].split('/')
+        var s = document.createElement('script')
+        s.id = names[names.length - 1]
+        s.type = 'text/javascript'
+        s.innerHTML = a
+        document.head.appendElement(s)
+        loadJs(js, index++)
+      }
+    })
+  }
+  _get({
+    url: './load.json',
+    done: function (data) {
+      var b = ''
+      const head = document.getElementById('head')
+      data.css.forEach(function (a) {
+        head.innerHTML += '<link rel="stylesheet" href="' + a + '">'
+      })
+      loadJs(data.js, 0)
+    }
+  })
 })();
