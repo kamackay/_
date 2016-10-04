9839bc8522af99234a08c78b4643a3c4cc6f2288
Keith MacKay <kamackay@ncsu.edu>
2016-10-04 16:57:38 -0400

    new resume work

--- resume_new/load.js
+++ resume_new/load.js
@@ -1,28 +1,51 @@
 (function () {
-  var loadJs = function (js, index) {
-    index = index || 0
-    _get({
-      url: js[index],
-      done: function (a) {
-        var names = js[index].split('/')
-        var s = document.createElement('script')
-        s.id = names[names.length - 1]
-        s.type = 'text/javascript'
-        s.innerHTML = a
-        document.head.appendElement(s)
-        loadJs(js, index++)
-      }
-    })
-  }
-  _get({
-    url: './load.json',
-    done: function (data) {
-      var b = ''
-      const head = document.getElementById('head')
-      data.css.forEach(function (a) {
-        head.innerHTML += '<link rel="stylesheet" href="' + a + '">'
-      })
-      loadJs(data.js, 0)
+    var html_src;
+
+    var loadJs = function (js, index) {
+        try {
+            if (index >= js.length) {
+                console.log("scripts fully loaded");
+                if (html_src) {
+                    _get({
+                        url: html_src,
+                        done: function (html) {
+                            $('#loading').slideUp(1000, function () {
+                                document.body.innerHTML = html;
+                            })
+                        }
+                    });
+                }
+                return;
+            }
+            _get({
+                url: js[index],
+                done: function (a) {
+                    var names = js[index].split('/');
+                    var s = document.createElement('script');
+                    s.id = names[names.length - 1].split('.')[0];
+                    s.type = 'text/javascript';
+                    s.innerHTML = a;
+                    document.head.appendChild(s);
+                    loadJs(js, index + 1);
+                },
+                fail(info) {
+                    console.log("error loading " + js[index]);
+                }
+            })
+        } catch (error) {
+            console.log("error loading " + js[index]);
+        }
     }
-  })
+    _get({
+        url: './load.json',
+        done: function (data) {
+            const head = document.getElementById('head');
+            data.css.forEach(function (a) {
+                head.innerHTML += '<link rel="stylesheet" href="' + a + '">'
+            });
+            loadJs(data.js, 0);
+            if (data.html_src) html_src = data.html_src;
+            if (data.background_src) document.body.style.backgroundImage = "url(" + data.background_src + ")";
+        }
+    })
 })();
